83f814c0ab5bc773581ebe268cd917dff12ba7b5
altship <1376552345@qq.com>
2024-12-20 12:36:01 +0800

    Moments app created and completed.

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export BKPAAS_APP_ID=bk-saas-deploy
 export BKPAAS_APP_SECRET=MWkY0JydSdszKfvuQXqy3jGnWbDcGCzQinlx
 export BKPAAS_MAJOR_VERSION=3
@@ -5,3 +6,5 @@ export BK_PAAS2_URL=https://ce.bktencent.com
 export BK_COMPONENT_API_URL=https://bkapi.ce.bktencent.com
 export BKPAAS_LOGIN_URL=https://ce.bktencent.com/login/
 export CORS_ALLOWED_ORIGIN=http://dev.ce.bktencent.com:5000
+
+python3 manage.py runserver dev.ce.bktencent.com:5000
---
+++ moments/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import WeChatUser, Status
+admin.site.register(WeChatUser)
+admin.site.register(Status)
